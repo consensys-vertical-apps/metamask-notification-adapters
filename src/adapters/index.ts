a7e3f7efbdadb75f5098378a634f6403f2aa8a0f
Tamas <soostamas.hu@gmail.com>
2024-09-03 14:38:07 +0200

    feat: scaff

---
+++ src/adapters/index.ts
@@ -0,0 +1,82 @@
+import * as test_is_matching from "#/adapters/test/is_matching";
+import * as test_is_not_matching from "#/adapters/test/is_not_matching";
+import * as test_is_not_active_user from "#/adapters/test/is_not_active_user";
+import * as test_is_active_user from "#/adapters/test/is_active_user";
+import * as test_is_not_supported_chain from "#/adapters/test/is_not_supported_chain";
+import * as domain from "#/domain";
+import type * as viem from "viem";
+
+export const CONTRACT_ADAPTERS: ContractAdapters = {
+    // Test adapters
+    [domain.Kind.TestIsMatching]: new test_is_matching.Adapter(),
+    [domain.Kind.TestIsNotMatching]: new test_is_not_matching.Adapter(),
+    [domain.Kind.TestIsActiveUser]: new test_is_active_user.Adapter(),
+    [domain.Kind.TestIsNotActiveUser]: new test_is_not_active_user.Adapter(),
+    [domain.Kind.TestIsNotSupportedChain]: new test_is_not_supported_chain.Adapter(),
+};
+
+type ContractAdapters = {
+    // biome-ignore lint/suspicious/noExplicitAny: <explanation>
+    [k in domain.Kind]?: IContractAdapter<any, any, any>;
+};
+
+export interface IContractAdapter<U, S, C> {
+    checkUser: (address: viem.Address, chainId: domain.Chain, client: viem.PublicClient) => Promise<UserCheckResult<U>>;
+    matchTrigger: (trigger: domain.Trigger<U, S>, client: viem.PublicClient) => Promise<MatchResult<S, C>>;
+    mapIntoNotificationData: (trigger: domain.Trigger<U, S>, context: C) => Promise<domain.NotificationData>;
+}
+
+export type UserCheckResult<U> = ActiveUserResult<U> | NotActiveUserResult;
+
+interface ActiveUserResult<U> {
+    active: true;
+    userSettings?: U;
+    error?: never;
+}
+
+interface NotActiveUserResult {
+    active: false;
+    userSettings?: never;
+    error?: Error;
+}
+
+export type MatchResult<S, C> = MatchedResult<S, C> | UnmatchedResult<S>;
+
+interface MatchedResult<S, C> {
+    matched: true;
+    dedupKey?: string;
+    state?: S;
+    context: C;
+    error?: never;
+}
+
+interface UnmatchedResult<S> {
+    matched: false;
+    state?: S;
+    context?: never;
+    error?: Error;
+}
+
+export class NotActiveUserError extends Error {
+    constructor() {
+        super("not active user");
+        this.name = "NotActiveUserError";
+        Object.setPrototypeOf(this, NotActiveUserError.prototype);
+    }
+
+    toJSON(): string {
+        return this.message;
+    }
+}
+
+export class NotSupportedChainError extends Error {
+    constructor() {
+        super("not supported chain");
+        this.name = "NotSupportedChainError";
+        Object.setPrototypeOf(this, NotSupportedChainError.prototype);
+    }
+
+    toJSON(): string {
+        return this.message;
+    }
+}
